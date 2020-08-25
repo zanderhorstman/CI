3602b0e86ee9283ed2b9679c24d464660941d30f
zanderhorstman <zanderhorstman@gmail.com>
2020-08-25 16:04:21 -0400

    1.00

---
+++ src/CI.java
@@ -0,0 +1,15 @@
+public class CI {
+    public static void main(String[] args) {
+
+        int workOutDays = 10;
+        double health = 8.5;
+        boolean powerUp = true;
+
+        System.out.println("Number of days: " + workOutDays);
+        System.out.println("Health Scale: " + health);
+        System.out.println("Are you ready to power up? " + powerUp);
+        System.out.println("Total Assessment: It has been " + workOutDays + " days since you've started working out. ");
+        System.out.println("You evaluate at an " + health + " out of 10 on the health scale.");
+        System.out.println("It is " + powerUp + " that you are ready to power up your workout!");
+    }
+}
